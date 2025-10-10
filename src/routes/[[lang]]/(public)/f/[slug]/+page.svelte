7a76200bf5e5f76010ae03b3e44f5342d38d501e
Guy Lahav <guylahav@machine.local>
2025-10-10 08:49:40 +0300

    feat: data load for public form signing route

--- src/routes/[[lang]]/(public)/f/[slug]/+page.svelte
+++ src/routes/[[lang]]/(public)/f/[slug]/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import BasePage from '$lib/components/base-page/base-page.svelte';
-	import type { UserForm } from '$lib/server/database/schemas/form';
-	const form: UserForm = $derived(page.data.data);
+	import type { FormTemplate, UserForm } from '$lib/server/database/schemas/form';
+	const form: UserForm = $derived(page.data.form);
+	const schema: FormTemplate['schema'] = $derived(page.data.schema);
 </script>
 
 <BasePage title="common.sign" description="seo.description">
--- src/routes/[[lang]]/(public)/f/[slug]/+page.ts
+++ src/routes/[[lang]]/(public)/f/[slug]/+page.ts
@@ -1,19 +1,28 @@
 import { GET } from '$lib/api/helpers/request';
 import type { FormTemplate, UserForm } from '$lib/server/database/schemas/form';
+import { error } from '@sveltejs/kit';
 import { FormsTemplates, UsersForms } from '../../../../api';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params, fetch }) => {
+	const response: { schema: FormTemplate['schema'] | null; form: Pick<UserForm, 'id'> | null } = {
+		schema: null,
+		form: null
+	};
 	const publicLinkIndetifier = params.slug;
-	const userForm = await GET<UserForm>(`${UsersForms}?pli=${publicLinkIndetifier}&limit=1`, {
+	const [userForm] = await GET<UserForm[]>(`${UsersForms}?pli=${publicLinkIndetifier}&limit=1`, {
 		fetch
 	});
-	const template = await GET<FormTemplate>(`${FormsTemplates}/${userForm.template_id}`, { fetch });
+	if (!userForm) {
+		error(404);
+	}
+	response.form = { id: userForm.id };
 
-	return {
-		schema: template.schema,
-		form: {
-			id: userForm.id
-		}
-	};
+	const template = await GET<FormTemplate>(`${FormsTemplates}/${userForm.template_id}`, { fetch });
+	if (!template) {
+		error(500);
+	}
+	response.schema = template.schema;
+
+	return response;
 };
