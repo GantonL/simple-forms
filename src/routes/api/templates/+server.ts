5fcd0b9dde778a932f5caf2eacfe2c9322b0cee9
Guy Lahav <guylahav@machine.local>
2025-11-08 19:25:08 +0200

    fix: remove redudant routes from templates controller

--- src/routes/api/templates/+server.ts
+++ src/routes/api/templates/+server.ts
@@ -1,10 +1,7 @@
 import {
 	Service as service,
 	getUrlFilters,
-	getUrlOptions,
-	buildCreateCandidates,
-	buildUpdateData,
-	getBodyFilters
+	getUrlOptions
 } from '$lib/server/database/services/form-templates';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
@@ -13,28 +10,4 @@ export const GET: RequestHandler = async ({ url }) => {
 	const options = getUrlOptions(url);
 	const items = await service.find(filters, options);
 	return json(items);
-};
-
-export const POST: RequestHandler = async ({ request }) => {
-	const { data } = await request.json();
-	const itemsToCreate = buildCreateCandidates(data);
-	const created = await service.createMany(itemsToCreate);
-	return json({ created });
-};
-
-export const PUT: RequestHandler = async ({ url, request }) => {
-	const { data, filters } = await request.json();
-	const urlFilters = getUrlFilters(url);
-	const bodyFilters = getBodyFilters(filters);
-	const updateData = buildUpdateData(data);
-	const updated = await service.updateWhere([...urlFilters, ...bodyFilters], updateData);
-	return json({ updated });
-};
-
-export const DELETE: RequestHandler = async ({ url, request }) => {
-	const { filters } = await request.json();
-	const urlFilters = getUrlFilters(url);
-	const bodyFilters = getBodyFilters(filters ?? []);
-	const deleted = await service.deleteWhere([...urlFilters, ...bodyFilters]);
-	return json({ deleted });
 };
