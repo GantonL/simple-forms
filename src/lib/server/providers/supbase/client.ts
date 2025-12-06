931ced575c575d77d854dcccf22c9cd8ebef7d65
Guy Lahav <guylahav@machine.local>
2025-12-06 13:56:06 +0200

    fix: supabase apikey name

--- src/lib/server/providers/supbase/client.ts
+++ src/lib/server/providers/supbase/client.ts
@@ -1,4 +1,4 @@
 import { env } from '$env/dynamic/private';
 import { createClient } from '@supabase/supabase-js';
 
-export const client = createClient(env.SUPBASE_PROJECT_URL, env.SUPABAE_API_KEY);
+export const client = createClient(env.SUPBASE_PROJECT_URL, env.SUPABASE_API_KEY);
