01dfbf39b84fd4597f7a2919634ce1902eca77e4
Guy Lahav <guylahav@machine.local>
2025-12-26 16:43:13 +0200

    fix: upload directy from client with a signeduploadurl

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -4,4 +4,5 @@ export const ManageCookies = `${CONTROLLER}/manage-cookies`;
 export const FormsTemplates = `${CONTROLLER}/templates`;
 export const UsersForms = `${CONTROLLER}/users-forms`;
 export const FormsSubmissions = `${CONTROLLER}/forms-submissions`;
+export const UploadUrl = `${CONTROLLER}/upload-url`;
 export const SignedUrl = (id: number) => `${CONTROLLER}/storage/${String(id)}/signed-url`;
