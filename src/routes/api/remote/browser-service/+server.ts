663c9314468c394db1841fd11ba65984c90f68b9
Guy Lahav <guylahav@machine.local>
2025-12-29 20:07:21 +0200

    fit: build browser serice url from env vars

--- src/routes/api/remote/browser-service/+server.ts
+++ src/routes/api/remote/browser-service/+server.ts
@@ -1,12 +1,13 @@
+import { BROWSER_SERVICE_HOST, BROWSER_SERVICE_PORT } from '$env/static/private';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
-	const res = await fetch('http://remote-browser-service.railway.internal:3001/health').catch(
-		(e) => {
-			console.error(e);
-			return;
-		}
-	);
+	const browserServiceUrl = `http://${BROWSER_SERVICE_HOST}:${BROWSER_SERVICE_PORT}`;
+	console.log('browser service url', browserServiceUrl);
+	const res = await fetch(`${browserServiceUrl}/health`).catch((e) => {
+		console.error(e);
+		return;
+	});
 	console.log(res);
 	return json({ healthy: res?.ok });
 };
