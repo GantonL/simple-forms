655c80419821f515a09418fb6184ff3aac78a9a6
Guy Lahav <guylahav@machine.local>
2026-01-11 21:43:12 +0200

    fix: use requst json instead of readable stream

--- src/routes/api/remote/browser-service/pdf/+server.ts
+++ src/routes/api/remote/browser-service/pdf/+server.ts
@@ -1,9 +1,8 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 import { requestPdfCreation } from '$lib/server/remote/services/remote-browser';
 export const POST: RequestHandler = async ({ request }) => {
-	const data = (await request.body?.getReader().read())?.value?.toString();
-	if (!data) return json({ success: false });
-	const body = JSON.parse(data);
+	const body = await request.json();
+	if (!body) return json({ success: false });
 	const requested = await requestPdfCreation({
 		formPublicLinkIndentifier: body.data.formPublicLinkIndentifier,
 		formId: body.data.formId,
