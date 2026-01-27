7ae315379d162762ced968c98b41c34516f38bc9
Guy Lahav <guylahav@machine.local>
2026-01-27 22:08:02 +0200

    fix: remove reduduant import

--- src/routes/[[lang]]/(application)/forms/[form_id]/configurations.ts
+++ src/routes/[[lang]]/(application)/forms/[form_id]/configurations.ts
@@ -5,7 +5,7 @@ import type { ColumnDef } from '@tanstack/table-core';
 import CreateFormSubmissionLink from './create-form-submission-link.svelte';
 import { defaultDateCell } from '$lib/components/app-data-table/configurations/defaults';
 import { t } from '$lib/i18n';
-import { Cog, Component, Pencil } from '@lucide/svelte';
+import { Cog, Pencil } from '@lucide/svelte';
 
 export const DEFAULT_ORDER_BY = '-createdAt';
 
