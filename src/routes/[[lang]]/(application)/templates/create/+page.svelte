519b85e30160a735fc8490c042f22e9e5891e993
Guy Lahav <guylahav@machine.local>
2026-02-07 13:36:13 +0200

    feat: somewhat working dnd canvas

---
+++ src/routes/[[lang]]/(application)/templates/create/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import BasePage from '$lib/components/base-page/base-page.svelte';
+	import TemplateBuilder from '$lib/components/template-builder/template-builder.svelte';
+	import { t } from '$lib/i18n';
+</script>
+
+<BasePage title="common.template_builder.title" description="common.template_builder.description">
+	{#snippet header()}
+		<h2 class="text-2xl font-bold">{$t('common.template_builder.title')}</h2>
+		<p class="text-lg font-light">{$t('common.template_builder.description')}</p>
+	{/snippet}
+	<TemplateBuilder />
+</BasePage>
